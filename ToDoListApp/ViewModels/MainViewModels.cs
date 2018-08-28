acc13cc1ad447ae2d94d4b3b7705b00204580bc2
Jordan Kennedy <G00348844@gmit.ie>
2018-08-28 17:25:09 +0100

    Making the Task Name, Add Task Button work.

---
+++ ToDoListApp/ViewModels/MainViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp.ViewModels
+{
+	class MainViewModel
+	{
+		public string EnteredTaskName { get; set; }
+		public List<TaskViewModels> Tasks {get; set;}
+	}
+
+	internal class TaskViewModels
+	{
+		public string TaskName { get; set; }
+		public bool IsDone { get; set; }
+
+	}
+}
